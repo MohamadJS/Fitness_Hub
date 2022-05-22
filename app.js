02816b3b04e28cf65559780ae2676d58577cbced
MohamadJS <abdu.mohamad.17@gmail.com>
2022-05-22 15:05:01 -0500

    fixed cookies

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ const sessionConfig = {
     cookie: {
         httpOnly: true,
         sameSite: "strict",
-        secure: true,
+        // secure: true,
         expires: Date.now() * 1000 * 60 * 60 * 24,
         maxAge: 1000 * 60 * 60 * 24
     }
